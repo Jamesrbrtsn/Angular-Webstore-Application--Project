d6e935757123ace6482f5d9ece24c86ff6147536
James Robertson <James.M.Robertson.Work@gmail.com>
2019-05-28 16:22:45 -0400

    New Tutorial (Refresh)

--- server/config.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = {
-    AUTH0_DOMAIN: 'dev-zl72yk-1.auth0.com',
-    AUTH0_API_AUDIENCE: 'http://localhost:8083/api/',
-    MONGO_URI: 'mongodb://wookieCoookie:media628bawdvwa62v@ds263146.mlab.com:63146/webstore-project'
-};

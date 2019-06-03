3a3d92a50f1c161c85b976f9163eb60887ad08d3
James Robertson <James.M.Robertson.Work@gmail.com>
2019-06-03 17:35:29 -0400

    Removed Prior Server Project

--- malcolm-co-backend - a1/routes/storeItem.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Require the controllers WHICH WE DID NOT CREATE YET!!
-const storeItem_controller = require('../controllers/storeItem.controller');
-// a simple test url to check that all of our files are communicating correctly.
-router.get('/test', storeItem_controller.test);
-module.exports = router;
